fd523914b2c9c8df82ca094f00fe5d51097dc3be
Matheus Araujo Ferreira <matheus10go2014@gmail.com>
2024-03-22 03:49:48 -0300

    changes

--- app.js
+++ app.js
@@ -2,7 +2,5 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Funcionou'));
-
-app.listen(port);
+app.get('/', (req, r
 console.log(`App running on http://localhost:${port}`);
